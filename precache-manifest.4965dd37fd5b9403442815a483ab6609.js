18be5ac8f72ddb912be38702838f7d4d3f0f8e90
Aaron Lin <aaron_lin@wistron.com>
2019-08-13 11:44:35 +0800

    Updates

---
+++ precache-manifest.4965dd37fd5b9403442815a483ab6609.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6c56c9a29e4f324d31f62ebcf733b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c0b10e60c09aa20b7c",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "b8543e0437f0fdcdee67",
+    "url": "/static/js/2.f5c1b060.chunk.js"
+  },
+  {
+    "revision": "f7c14a023a3d2839607e",
+    "url": "/static/js/3.8f7a977f.chunk.js"
+  },
+  {
+    "revision": "a8928ba1eae7546529da",
+    "url": "/static/js/4.bf4baa93.chunk.js"
+  },
+  {
+    "revision": "97848d17682e01ef48ab",
+    "url": "/static/js/5.67669ad2.chunk.js"
+  },
+  {
+    "revision": "fadbdbe32a2301f6b62c",
+    "url": "/static/js/6.13ee9517.chunk.js"
+  },
+  {
+    "revision": "54aa5cb491fe71fb0852",
+    "url": "/static/js/7.b4ed9957.chunk.js"
+  },
+  {
+    "revision": "3b7fef0d722223725e80",
+    "url": "/static/js/8.2217021d.chunk.js"
+  },
+  {
+    "revision": "40c0b10e60c09aa20b7c",
+    "url": "/static/js/main.242675ea.chunk.js"
+  },
+  {
+    "revision": "b0637922b1ec2a35bc64",
+    "url": "/static/js/runtime~main.2ac29729.js"
+  }
+]);
