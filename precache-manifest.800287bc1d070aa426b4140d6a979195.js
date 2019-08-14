82c3c6512653dc50ca76efe34fcbaf684b048c4d
Aaron Lin <aaron_lin@wistron.com>
2019-08-14 12:56:44 +0800

    Updates

---
+++ precache-manifest.800287bc1d070aa426b4140d6a979195.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feae09a76b17b31274b933ce9d738d54",
+    "url": "/react_16.9_tutorials/index.html"
+  },
+  {
+    "revision": "18a13c34477206485223",
+    "url": "/react_16.9_tutorials/static/css/3.525f6c7e.chunk.css"
+  },
+  {
+    "revision": "1de49415245b4fb1a8d0",
+    "url": "/react_16.9_tutorials/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "6b4dbb5859e8c8ec468a",
+    "url": "/react_16.9_tutorials/static/js/10.d3d22380.chunk.js"
+  },
+  {
+    "revision": "391c53cd432e9d024cdb",
+    "url": "/react_16.9_tutorials/static/js/11.975ef3c0.chunk.js"
+  },
+  {
+    "revision": "b686d4c106dbb0931a2e",
+    "url": "/react_16.9_tutorials/static/js/12.c457d531.chunk.js"
+  },
+  {
+    "revision": "6ba9055665027d75c5ed",
+    "url": "/react_16.9_tutorials/static/js/13.383fd048.chunk.js"
+  },
+  {
+    "revision": "68cdcd8be6c56ece790f",
+    "url": "/react_16.9_tutorials/static/js/14.54fed68f.chunk.js"
+  },
+  {
+    "revision": "b8543e0437f0fdcdee67",
+    "url": "/react_16.9_tutorials/static/js/2.f5c1b060.chunk.js"
+  },
+  {
+    "revision": "18a13c34477206485223",
+    "url": "/react_16.9_tutorials/static/js/3.3a3c6ea3.chunk.js"
+  },
+  {
+    "revision": "d24b36999aaab049c616",
+    "url": "/react_16.9_tutorials/static/js/4.0269cb4a.chunk.js"
+  },
+  {
+    "revision": "e76973cfbab257a2656e",
+    "url": "/react_16.9_tutorials/static/js/5.91598150.chunk.js"
+  },
+  {
+    "revision": "7ce37f8ab3e9b147a900",
+    "url": "/react_16.9_tutorials/static/js/6.29296fcb.chunk.js"
+  },
+  {
+    "revision": "d7442000224c3d9a8983",
+    "url": "/react_16.9_tutorials/static/js/7.9d839b2a.chunk.js"
+  },
+  {
+    "revision": "ba942dfbdc9a9d50cd11",
+    "url": "/react_16.9_tutorials/static/js/8.6ad0b9e5.chunk.js"
+  },
+  {
+    "revision": "9c00cff4bfa59d0594d5",
+    "url": "/react_16.9_tutorials/static/js/9.b8b850ea.chunk.js"
+  },
+  {
+    "revision": "1de49415245b4fb1a8d0",
+    "url": "/react_16.9_tutorials/static/js/main.0f35e0f7.chunk.js"
+  },
+  {
+    "revision": "01e403a3c9b33a3d8a6c",
+    "url": "/react_16.9_tutorials/static/js/runtime~main.20bab7a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95866fad3805c8a4891c3948eba88d37.js"
+  "/react_16.9_tutorials/precache-manifest.800287bc1d070aa426b4140d6a979195.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_16.9_tutorials/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
